5ffe173d79e391f5984b7713bcc2ef81c68f4fbe
caoweiwei <43095369+caoweiwei@users.noreply.github.com>
2021-12-27 21:29:23 +0800

    refine

--- src/main/java/net/sf/jsqlparser/transform/rule/manager/RuleItemCollector.java
+++ src/main/java/net/sf/jsqlparser/transform/rule/manager/RuleItemCollector.java
@@ -12,41 +12,39 @@ import org.reflections.util.FilterBuilder;
 
 import java.lang.reflect.Method;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class RuleItemCollector {
-    private static Map<SQLEngine, List<AbstractRuleItem>> sqlRuleMap = new HashMap<>();
+    private static List<AbstractRuleItem> abstractRuleItemList = Lists.newArrayList();
 
     public static Map<SQLEngine, List<AbstractRuleItem>> getSqlRuleMap() {
-        return sqlRuleMap;
+        return abstractRuleItemList
+                .stream()
+                .collect(Collectors.groupingBy(abstractRuleItem -> abstractRuleItem.sqlEngine));
     }
 
     public static void init() {
         List<ClassLoader> classLoadersList = Lists.newArrayList(ClasspathHelper.contextClassLoader(), ClasspathHelper.staticClassLoader());
 
-        for (SQLEngine sqlEngine : SQLEngine.values()) {
+        Reflections reflections = new Reflections(new ConfigurationBuilder()
+                .setScanners(new SubTypesScanner(false), new ResourcesScanner())
+                .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                .filterInputsBy(new FilterBuilder().includePackage("net.sf.jsqlparser.transform.rule.config.dialect.*")));
 
-            Reflections reflections = new Reflections(new ConfigurationBuilder()
-                    .setScanners(new SubTypesScanner(false), new ResourcesScanner())
-                    .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
-                    .filterInputsBy(new FilterBuilder().includePackage(String.format("net.sf.jsqlparser.transform.rule.config.dialect.%s", sqlEngine.name().toLowerCase()))));
+        Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
 
-            Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
-
-            List<AbstractRuleItem> ruleItemList = Lists.newArrayList();
-            classes.forEach(clazz -> {
-                try {
-                    Method[] methods = clazz.getMethods();
-                    for (Method method : methods) {
-                        if (method.getName().startsWith("getItem")) {
-                            ruleItemList.add((AbstractRuleItem)method.invoke(null));
-                        }
+        classes.forEach(clazz -> {
+            try {
+                Method[] methods = clazz.getMethods();
+                for (Method method : methods) {
+                    if (method.getName().startsWith("getItem")) {
+                        abstractRuleItemList.add((AbstractRuleItem)method.invoke(null));
                     }
-                } catch (Exception e) {
-                    log.error("SQLEngine:{}, clazz:{}, getItem invoke error.", sqlEngine.name(), clazz.getSimpleName());
                 }
-            });
-            sqlRuleMap.put(sqlEngine, ruleItemList);
-        }
+            } catch (Exception e) {
+                log.error("clazz:{}, getItem invoke error.", clazz.getSimpleName());
+            }
+        });
     }
 }
